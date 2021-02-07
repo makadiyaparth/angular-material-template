a95e20cb15be686ca73bea1e51c1e017f76b4498
Parth Makadiya <makadiyaparth98@gmail.com>
2021-02-07 12:02:02 +0530

    add core & material module

---
+++ src/app/core/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+
+
+const materialModules = [];
+
+@NgModule({
+  declarations: [],
+  imports: materialModules,
+  exports: materialModules
+})
+export class MaterialModule { }
